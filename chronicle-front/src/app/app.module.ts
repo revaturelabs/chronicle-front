a1e87f7d8414c3819385a6dc928d297796dd1bba
Justin Kroh <justinkroh22@gmail.com>
2020-12-23 18:39:04 -0500

    Added working login logout on angular side

--- chronicle-front/src/app/app.module.ts
+++ chronicle-front/src/app/app.module.ts
@@ -9,17 +9,88 @@ import { LoginComponent } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC0-bDLBHeSIVzq2HblBWhaEjAQM1vj4Tw",
+  authDomain: "test-a0bf1.firebaseapp.com",
+  projectId: "test-a0bf1",
+  storageBucket: "test-a0bf1.appspot.com",
+  messagingSenderId: "512874803586",
+  appId: "1:512874803586:web:2ead92cb69b4b8345b1f9b",
+  measurementId: "G-BEDMWJEGEF"
+};
+
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  
+  
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        'auth_type': 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+};
+
+
+const firebaseUiAuthConfig2: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  
+  
+  signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+
+  ],
+  tosUrl: 'https://finance.yahoo.com/',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomepageComponent
+    HomepageComponent,
+    NavbarComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- chronicle-front/src/app/components/login/login.component.ts
+++ chronicle-front/src/app/components/login/login.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+// import * as firebaseui from 'firebaseui';
+// import * as firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult } from 'firebaseui-angular';
+
+
+// var firebase = require('firebase');
+// var firebaseui = require('firebaseui');
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+
+    this.afAuth.currentUser
+  } 
+
+  successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult) {
+    
+    console.log("I signed in" + signInSuccessData.authResult.user?.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+      // Send token to your backend via HTTPS
+
+      console.log(idToken);
+      // ...
+    }).catch(function(error) {
+      // Handle error
+    }));
+
+
+
+  }
+
+  errorCallback(errorData: FirebaseUISignInFailure) {
+    console.log("I am a failure" + errorData);
+
+
+  }
+  uiShownCallback() {
+     console.log("on loading of login ui");
+  }
 
   ngOnInit(): void {
+
+
   }
+
+  logout(){
+    this.afAuth.signOut();
+    console.log("you logged out");
+
+  }
+
 
 }
