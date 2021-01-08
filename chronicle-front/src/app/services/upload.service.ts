bb208e409327c8e41ac1d4d723413db423dda3dd
John Paul Sandoz desktop <jsando6@lsu.edu>
2021-01-08 09:59:24 -0600

    adding form data module, upload button works, unsure if it sends data

--- chronicle-front/src/app/services/upload.service.ts
+++ chronicle-front/src/app/services/upload.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, retry } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
@@ -30,19 +30,45 @@ export class UploadService {
 /*
 this makes the call to the Spring boot Application
 */
-  upload(data:any) {
-    console.log(data.title);
-    this.http.post('${this.baseURL}/submit', data, {observe: 'response'})
-    .subscribe((response) => {
-      if (response.status ===200){
-        console.log('upload successful')
-      } else {
-        console.log('upload unsuccessful')
-      }
-    }
-    );
-  }
+  // upload(data:any) {
+  //   console.log(data.title);
+  //   this.http.post('${this.baseURL}/submit', data, {observe: 'response'})
+  //   .subscribe((response) => {
+  //     if (response.status ===200){
+  //       console.log('upload successful')
+  //     } else {
+  //       console.log('upload unsuccessful')
+  //     }
+  //   }
+  //   );
+  // }
     
+
+  /*
+  This method uses FormData to append the name of the file into a key value pair.
+  FormData is a structure that is used to store infformation in key-value pairs.
+  The append() method writes a new value onto the existing key inside the FormData object or
+  adds a key if it does not already exist.
+  */
+ upload(file: File): Observable<HttpEvent<any>>{
+   const formData: FormData = new FormData();
+   formData.append('file',file);
+
+   /*
+   The HttpClient allows us to send a POST request to the spring boot server.
+   FormData also allows us to use a feature called reportProgress to show the status of uploading a file,
+   however, using this is expensive since it changes detection for each event.
+   */
+   const req = new HttpRequest('POST', `${this.baseURL}/submit`, formData,{
+     reportProgress: true,
+     responseType: 'json'
+   });
+   return this.http.request(req);
+ }
+ //unsure if this should even be implemented 
+ getFiles(): Observable<any>{
+   return this.http.get(`${this.baseURL}/files`);
+ }
 
   // Error handling
   errorHandl(error: any) {
