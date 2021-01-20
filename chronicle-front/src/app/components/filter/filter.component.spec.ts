b1e466e1b1449a4724d645755f5ecd9129bcc625
justinkroh22 <justinkroh22@gmail.com>
2021-01-20 16:50:01 -0500

    karma fix

--- chronicle-front/src/app/components/filter/filter.component.spec.ts
+++ chronicle-front/src/app/components/filter/filter.component.spec.ts
@@ -1,4 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 import { FilterComponent } from './filter.component';
 
@@ -6,12 +12,17 @@ describe('FilterComponent', () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FilterComponent ]
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+        imports: [
+            RouterTestingModule,
+            AngularFireModule.initializeApp(environment.firebaseConfig),
+            HttpClientModule,
+        ],
+        declarations: [NavbarComponent, MatAutocomplete],
     })
     .compileComponents();
-  });
+});
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FilterComponent);
@@ -19,7 +30,5 @@ describe('FilterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
