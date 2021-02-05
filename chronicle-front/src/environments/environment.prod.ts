b55f7edb7d6fc2d8b9c83361d121543e41a61041
George Yoo <gcyoosf@gmail.com>
2021-02-05 12:18:10 -0500

    adds environment variables to production environment

--- chronicle-front/src/environments/environment.prod.ts
+++ chronicle-front/src/environments/environment.prod.ts
@@ -1,3 +1,49 @@
+// export const environment = {
+//   production: true
+// };
+
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true
+  production: false,
+  // apiBase: 'http://localhost:8080/myapp',
+  apiBase: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp',
+  isSignedIn: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC4sxZlT-McTildwtxa8LV1lj7ZQhzOrs0',
+    authDomain: 'training-team-253916.firebaseapp.com',
+    projectId: 'training-team-253916',
+    storageBucket: 'training-team-253916.appspot.com',
+    messagingSenderId: '492701958610',
+    appId: '1:492701958610:web:4a30a1be93803701d3480b',
+    measurementId: 'G-DP6XDH9DTW'
+  },
+  serverApiUrls: {
+    getTags: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp/videos/available-tags',
+    getAllVideos: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp/videos/all',
+    getVideosByTag: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp/videos/tags/',
+    getVideoById: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp/videos/id/',
+    getAllNotes: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp/notes/all',
+    getNotesByTag: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp/notes/tags/',
+    getNoteById: 'http://3.236.46.62:8080/chronicle-0.0.1-SNAPSHOT/myapp/notes/id/'
+    // getTags: 'http://localhost:8080/myapp/videos/available-tags',
+    // getAllVideos: 'http://localhost:8080/myapp/videos/all',
+    // getVideosByTag: 'http://localhost:8080/myapp/videos/tags/',
+    // getVideoById: 'http://localhost:8080/myapp/videos/id/',
+    // getAllNotes: 'http://localhost:8080/myapp/notes/all',
+    // getNotesByTag: 'http://localhost:8080/myapp/notes/tags/',
+    // getNoteById: 'http://localhost:8080/myapp/notes/id/'
+  }
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
