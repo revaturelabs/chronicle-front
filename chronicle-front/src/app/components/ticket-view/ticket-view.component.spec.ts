74262f7e4688788a6117ea358911ad69b058423a
Lyon-Home <honpan.loi@gmail.com>
2021-03-28 15:46:54 -0500

    finish writing tests for view tickets

--- chronicle-front/src/app/components/ticket-view/ticket-view.component.spec.ts
+++ chronicle-front/src/app/components/ticket-view/ticket-view.component.spec.ts
@@ -17,12 +17,20 @@ export class MockTicketService extends TicketService{
     return mockTickets;
   }
 
-  findAllAcknowledgedTickets(){
+  findAllTicketsByEditor(){
     let mockTickets:Observable<Ticket[]> = of([
       new Ticket(1,1,100,"java primitives", "1 of 10", "00:45:56", "00:55:56","https://123", "11331345", 234, "ACKNOWLEDGED", "CR 2/26/2021", ""),
       new Ticket(3,5,100,"java interface", "3 of 10", "01:05:56", "01:20:56","https://123", "11331345", 234, "ACKNOWLEDGED", "CR 2/26/2021", "")
     ])
     return mockTickets;
+  }
+
+  updateTicketStatus(ticket:Ticket):Observable<Ticket>{
+
+    let mockTicket:Observable<Ticket> = of (new Ticket(1,1,100,"java primitives", "1 of 10", "00:45:56", "00:55:56","https://123", "11331345", 234, "ACKNOWLEDGED", "CR 2/26/2021", "")
+    )
+    
+    return mockTicket;
   }
 }
 
@@ -55,19 +63,37 @@ describe('TicketViewComponent', () => {
 
   });
 
-  it('should find ACKNOWLEDGED tickets', () =>{
-    component.findAllAcknowledgedTickets();
-    expect(component.allAcknowledgedTickets.length).toEqual(2);
+  it('should find all my tickets', () =>{
+    component.findAllMyTickets();
+    expect(component.allMyTickets.length).toEqual(2);
+    expect(component.allMyTickets[0].ticketStatus).toEqual("ACKNOWLEDGED")
 
   });
 
-  it('should acquire a list of videos uploaded up that user')
+  //not sure it it is possible
+  //it('should acquire a list of videos uploaded by that user')
 
-  it('should change the status of a ticket to ACKNOWLEDGED')
 
-  it('should change the status of a ticket to IN_PROGRESS')
+  it('should change the status of a ticket to ACKNOWLEDGED', () =>{
+    let testTicket:Ticket = new Ticket(3,5,100,"java class", "4 of 10", "01:45:56", "01:55:56","https://123", "11331345", 234, "PENDING", "CR 2/26/2021", "")
+    component.updateTicketStatusToAcknowledged(testTicket)
 
-  it('should change the status of a ticket to UNDER_REVIEW')
+    expect(component.tempTicket.ticketStatus).toEqual("ACKNOWLEDGED")
+  });
+
+  it('should change the status of a ticket to IN_PROGRESS', () =>{
+    let testTicket:Ticket = new Ticket(3,5,100,"java class", "4 of 10", "01:45:56", "01:55:56","https://123", "11331345", 234, "PENDING", "CR 2/26/2021", "")
+    component.updateTicketStatusToInProgress(testTicket)
+
+    expect(component.tempTicket.ticketStatus).toEqual("IN_PROGRESS")
+  });
+
+  it('should change the status of a ticket to UNDER_REVIEW', () =>{
+    let testTicket:Ticket = new Ticket(3,5,100,"java class", "4 of 10", "01:45:56", "01:55:56","https://123", "11331345", 234, "PENDING", "CR 2/26/2021", "")
+    component.updateTicketStatusToUnderReview(testTicket)
+
+    expect(component.tempTicket.ticketStatus).toEqual("UNDER_REVIEW")
+  });
 
 
 });
--- chronicle-front/src/app/components/ticket-view/ticket-view.component.ts
+++ chronicle-front/src/app/components/ticket-view/ticket-view.component.ts
@@ -17,7 +17,9 @@ export class TicketViewComponent implements OnInit {
   }
 
   allPendingTickets: Ticket[] = [];
-  allAcknowledgedTickets: Ticket[] = [];
+  allMyTickets: Ticket[] = [];
+
+  tempTicket:Ticket = new Ticket(0,0,0,"", "", "", "","", "", 0, "", "", "");
 
   findAllPendingTickets(){
     this.ticketService.findAllPendingTickets().subscribe(
@@ -30,10 +32,52 @@ export class TicketViewComponent implements OnInit {
     )
   }
 
-  findAllAcknowledgedTickets(){
-    this.ticketService.findAllPendingTickets().subscribe(
+  findAllMyTickets(){
+    this.ticketService.findAllTicketsByEditor().subscribe(
       (data) => {
-        this.allPendingTickets = data;
+        this.allMyTickets = data;
+      },
+      () => {
+
+      }
+    )
+  }
+
+  updateTicketStatusToAcknowledged(ticket:Ticket){
+    ticket.ticketStatus = "ACKNOWLEDGED"
+    this.tempTicket = ticket;
+
+    this.ticketService.updateTicketStatus(ticket).subscribe(
+      (data) => {
+        console.log("ticket has been updated"+data)
+      },
+      () => {
+
+      }
+    )
+  }
+
+  updateTicketStatusToInProgress(ticket:Ticket){
+    ticket.ticketStatus = "IN_PROGRESS"
+    this.tempTicket = ticket;
+
+    this.ticketService.updateTicketStatus(ticket).subscribe(
+      (data) => {
+        console.log("ticket has been updated"+data)
+      },
+      () => {
+
+      }
+    )
+  }
+
+  updateTicketStatusToUnderReview(ticket:Ticket){
+    ticket.ticketStatus = "UNDER_REVIEW"
+    this.tempTicket = ticket;
+
+    this.ticketService.updateTicketStatus(ticket).subscribe(
+      (data) => {
+        console.log("ticket has been updated"+data)
       },
       () => {
 
