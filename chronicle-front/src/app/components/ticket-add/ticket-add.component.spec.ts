4ae6cf1575465ec3893bc67a5e89d7bebe0cb371
DaniPad7 <padilladaniel777@gmail.com>
2021-03-29 12:08:15 -0700

    new ticket-add tests

--- chronicle-front/src/app/components/ticket-add/ticket-add.component.spec.ts
+++ chronicle-front/src/app/components/ticket-add/ticket-add.component.spec.ts
@@ -22,4 +22,30 @@ describe('TicketAddComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate Zoom URL', () => {
+    let url:String = 'https://revature.zoom.us/rec/share/thdhdehdethyjyj';
+    expect(component.zoomUrlValidator(url)).toBeTruthy();
+  });
+
+  it('should validate time stamps through order and format', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should increment row count through add more button click', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should validate the row count to reach a limit', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should decrease row count through delete button click', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call ticket service upon submit button click', () => {
+    expect(component).toBeTruthy();
+  });
+
 });
--- chronicle-front/src/app/components/ticket-add/ticket-add.component.ts
+++ chronicle-front/src/app/components/ticket-add/ticket-add.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-add.component.css']
 })
 export class TicketAddComponent implements OnInit {
-
+private zoomURL:String ='';
+private timeStampRowCount:number =0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  zoomUrlValidator(zoomUrl:String):boolean {
+    return zoomUrl.startsWith('https://revature.zoom.us/rec/share');
+  }
+
+  timeStampValidator() {
+
+  }
+
+  rowCountIncrementor() {
+
+  }
+
+  rowCountValidator() {
+
+  }
+
+  rowCountDecrementor() {
+
+  }
+
+  submitTimeStamps() {
+
   }
 
 }
