cfe8ef11e39a76cebdac6a00cd8db37d690aeb6d
ricokali <ricokali@ricokali.com>
2021-03-31 14:36:04 -0700

    drop down menu

--- chronicle-front/src/app/components/ticket-view/ticket-view.component.ts
+++ chronicle-front/src/app/components/ticket-view/ticket-view.component.ts
@@ -64,7 +64,7 @@ export class TicketViewComponent implements OnInit {
         console.log("ticket has been updated"+data)
       },
       () => {
-
+        console.log("Ticket Update Failed")
       }
     )
   }
@@ -78,7 +78,7 @@ export class TicketViewComponent implements OnInit {
         console.log("ticket has been updated"+data)
       },
       () => {
-
+        console.log("Ticket Update Failed")
       }
     )
   }
