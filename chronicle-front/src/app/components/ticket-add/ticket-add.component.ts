7ba44f0496371d55145f2820ac8024f19c8ee45d
Sofia-S <sofka.vs@gmail.com>
2021-04-06 15:33:04 -0500

    final touches for add ticket component

--- chronicle-front/src/app/components/ticket-add/ticket-add.component.ts
+++ chronicle-front/src/app/components/ticket-add/ticket-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, Directive, Input, OnInit } from '@angular/core';
-import { AbstractControl, FormsModule, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Ticket } from 'src/app/models/Ticket';
 import { AuthService } from 'src/app/services/auth.service';
 import { TicketService } from 'src/app/services/ticket.service';
@@ -26,12 +26,14 @@ user:any;
 ticket:Ticket  = new Ticket(0,'0','0',new Date(),new Date(),"","","","",this._zoomURL,this.passcode,0,"pending",this.identifier,"","");
 tickets:Ticket[] = [this.ticket];
 visibility:boolean = true;
-submitted:boolean = true;
 globalTimeFormat:boolean = false;
 globalTimeFormat2:boolean = false;
 globalTopic:boolean = false;
 globalZoomUrl:boolean = false;
 globalTimeStampOrder:boolean = false;
+success:string ="Ticket submitted successfully";
+error:string ="Could not submit ticket"
+action:string = "close";
 
 public get topicCountGetter() {
   return this._topicCount;
@@ -45,7 +47,7 @@ public get returnTicketGetter() {
   return this._returnTickets;
 }
 
-  constructor(private ticketService:TicketService, private authService:AuthService) { }
+  constructor(private ticketService:TicketService, private authService:AuthService,private _snackBar: MatSnackBar) { }
 
 
   ngOnInit(): void {
@@ -81,7 +83,7 @@ public get returnTicketGetter() {
   }
 
 
-
+// Validate format of Starting time
   timeStampFormatValidator(time:string):void {
     let regexp = new RegExp('[0-9]{2}:[0-5]{1}[0-9]{1}:[0-5]{1}[0-9]{1}');
     //check if both timestamps are valid
@@ -89,6 +91,7 @@ public get returnTicketGetter() {
     else this.globalTimeFormat = false;
   }
 
+  // Validate format of Ending time
   timeStampFormatValidator2(time:string):void {
     let regexp = new RegExp('[0-9]{2}:[0-5]{1}[0-9]{1}:[0-5]{1}[0-9]{1}');
     //check if both timestamps are valid
@@ -96,6 +99,7 @@ public get returnTicketGetter() {
     else this.globalTimeFormat2 = false;
   }
 
+  //Making sure that Ending time is going after Starting time
   timeStampOrderValidator(startTime:string, endTime:string):void {
     //parse the timestamp into numbers
     let x:number = parseInt(startTime.replace(":","").replace(":",""));
@@ -111,6 +115,7 @@ public get returnTicketGetter() {
       }
   }
 
+  //Validate topic input
   topicValidator(topic:string):boolean {
 
     //check if both timestamps are valid
@@ -124,6 +129,7 @@ public get returnTicketGetter() {
     }
   }
 
+  //Count quantity of clips
   topicCountIncrementor() {
     if (this.topicCountValidator()) {
       this.tickets.push(new Ticket(0,this.user.uid,'0',new Date(),new Date(),"","","","",this._zoomURL,this.passcode,0,"pending",this.identifier,"",""))
@@ -133,14 +139,14 @@ public get returnTicketGetter() {
     }
 
   }
+  topicCountDecrementor() {
+    this._topicCount--;
+  }
 
+  //Set limit of clips
   topicCountValidator():boolean{
     if(this._topicCount > 30) return false;
     else return true;
-  }
-
-  topicCountDecrementor() {
-    this._topicCount--;
   }
 
   //Make sure that each clip got last entered link , passcode and batch
@@ -160,26 +166,28 @@ public get returnTicketGetter() {
     });
   }
 
+  // Submit ticket button
   submitTickets() {
     this.checkLink();
     this.checkPasscode();
     this.checkBatch();
 
-  
     this.ticketService.submitTickets(this.tickets).subscribe(
       (data) => {
         this.tickets = data;
-        console.log('Successfully submitted tickets.');
-        //refresh page after succsess
-        this.submitted = false;
-        window.location.reload(),5000;
+        //display message and refresh page after succsess 
+        setTimeout(location.reload.bind(location), 5000);
+        this.openSnackBar(this.success,this.action);
       },
       () => {
-        console.log('Failure in submitting tickets.');
+        //display an error message
+        this.openSnackBar(this.success,this.action);
+       
       }
     )
   }
 
+  //button for delete individual clip
   deleteTopic(ticket:Ticket) {
     if(this.tickets.includes(ticket)) {
       const index = this.tickets.indexOf(ticket, 0);
@@ -191,27 +199,13 @@ public get returnTicketGetter() {
 
     }
   }
-
-}
-
-@Directive({
-  selector: '[appZoomUrlValidator]',
-  providers: [{provide: NG_VALIDATORS, useExisting: ZoomUrlValidatorDirective, multi: true}]
-})
-export class ZoomUrlValidatorDirective implements Validator {
-  @Input('appZoomUrlValidator') validatedUrl!: string;
-  // zoomUrlValidator(zoomUrl:string):boolean {
-  //   return zoomUrl.startsWith('https://revature.zoom.us/rec/share');
-  // }
-
-  validate(control: AbstractControl): ValidationErrors | null {
-    return !this.validatedUrl ? this.urlValidator(this.validatedUrl)(control): null;
+// Pop up message 
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 5000,
+    });
   }
 
-  urlValidator(url:string): ValidatorFn {
-  return (control: AbstractControl): {[key: string]: any} | null => {
-    const allowedUrl:boolean = control.value.startsWith(url);
-    return !allowedUrl ? {forbiddenName: {value: control.value}} : null;
-  };
 }
-}
+
+
